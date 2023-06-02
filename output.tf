73b8a10d76a66f8d4d5eabf8317716b95b0e5fbc
Laszlo Fogas <laszlo@gimlet.io>
2023-06-02 14:21:39 +0200

    Only password is new information

--- output.tf
+++ output.tf
@@ -1,13 +1,3 @@
 output "user_password" {
   value = postgresql_role.app_user.password
 }
-
-output "user" {
-  value = postgresql_role.app_user.name
-}
-
-output "dbname" {
-  value = postgresql_role.app_user.name
-}
-
-
